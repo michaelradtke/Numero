7dd00dd20ff5877452edb4c8a704c8a026ee0c24
Michael Radtke <mradtke@abigale.de>
2019-11-29 17:23:37 +0100

    Added build sequence messages to ci file

--- ci.sh
+++ ci.sh
@@ -1,4 +1,5 @@
 !/bin/bash
+echo "BUILDUNG AND RUNNNING TESTS"
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test | tee xcodebuild.log | xcpretty
+echo "BUILDING RELEASE"
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO | tee xcodebuild.log | xcpretty
-
