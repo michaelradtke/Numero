370e42de14a33f52ec9b56f1f05ef19d8ca0ecdd
Michael Radtke <mradtke@abigale.de>
2019-11-29 12:38:41 +0100

    Using xcpretty for ci console output

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
 !/bin/bash
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test | tee xcodebuild.log | xcpretty
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO | tee xcodebuild.log | xcpretty
 
