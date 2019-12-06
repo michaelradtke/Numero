0e105e1c6261797c159efb9b548ba2a09897ec55
Michael Radtke <mradtke@abigale.de>
2019-12-06 12:15:20 +0100

    d

--- ci.sh
+++ ci.sh
@@ -1,5 +1,5 @@
 !/bin/bash
 echo "BUILDUNG AND RUNNNING TESTS"
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test | tee xcodebuild.log | /usr/local/bin/xcpretty--test --no-color --no-utf
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test | tee xcodebuild.log | /usr/local/bin/xcpretty --test --no-color --no-utf
 echo "BUILDING RELEASE"
 xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO | tee xcodebuild.log | /usr/local/bin/xcpretty --simple --no-color --no-utf && exit ${PIPESTATUS[0]}
